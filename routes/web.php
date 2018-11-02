43ef1bb8918ffe0f93056a94a6ae82ecdc0fbef1
ThingEngineer <josh.lee.campbell@gmail.com>
2018-11-02 18:29:27 -0500

    add doc link

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,7 @@
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
+| https://laravel.com/docs/5.7/routing
 |
 */
 
